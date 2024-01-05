3e74d97308e596b8b98a27af2a98008562e64650
Ahzi <70834999+ahzi-pav@users.noreply.github.com>
2024-01-05 23:20:34 +0800

    Remove game logic that plays exaclty five rounds

--- script.js
+++ script.js
@@ -20,39 +20,3 @@ function playRound(playerSelection, computerSelection) {
         return `You win, ${player} beats ${computerSelection}!`
     } 
 };
-
-let gameCount = 0;
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    while(gameCount < 5) {
-        console.log(`Your score: ${playerScore}, computer score: ${computerScore}.`)
-        const player = prompt("Enter your choice:");
-        switch(player.toLowerCase()) {
-            case "rock":
-            case "paper":
-            case "scissors":
-                break;
-            default:    
-                console.log('Invalid selection, try again.');
-                continue;
-        }
-        const computer = getComputerChoice();
-        if (player === computer) {
-            continue;
-        }
-        const start = playRound(player, computer);
-        console.log(start);
-        if (start.substring(0, 7) === "You win") {
-            playerScore += 1;
-        } 
-        
-        if (start.substring(0, 8) === "You lose") {
-            computerScore += 1;
-        }
-        gameCount += 1;
-    }
-    console.log(`Your score: ${playerScore}, computer score: ${computerScore}.`)
-    playerScore > computerScore ? console.log("You win!") : console.log("You lose!");
-};
